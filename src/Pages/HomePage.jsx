d1be1043835a24a2b81b7ad706ab6052b13e55dd
Baptiste <110813312+fleurybaptiste@users.noreply.github.com>
2023-03-02 23:06:48 +0100

    Add files via upload

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import s from "../Pages/HomePage.module.scss";
+
+export default function Home() {
+    return (
+        <div className={s.main_container}>
+            <h1>
+                Bienvenue sur notre site de jeux de rôle en français !<br />
+                Nous sommes fiers de proposer une large sélection de jeux de
+                rôle passionnants, conçus pour les joueurs en solo.
+                <br /> Que vous soyez un joueur chevronné ou que vous découvriez
+                le monde fascinant des jeux de rôle, notre site a quelque chose
+                à offrir à tous les amateurs de jeux de rôle.
+                <br /> Nous croyons que chacun mérite une expérience de jeu de
+                rôle immersive et captivante, et c'est pourquoi nous avons
+                rassemblé une collection de jeux de rôle solo passionnants, tous
+                traduits en français pour votre plaisir de lecture. Que vous
+                soyez à la recherche de jeux de rôle fantastiques, d'aventures
+                mystérieuses ou de simulations réalistes, nous avons tout ce
+                qu'il vous faut pour vous plonger dans un monde de jeux de rôle
+                fascinant.
+                <br /> Parcourez notre catalogue de jeux de rôle solo en
+                français et laissez-vous transporter dans des mondes imaginaires
+                riches en aventures, en énigmes et en personnages fascinants.
+                <br />
+                Nous sommes convaincus que vous trouverez quelque chose qui vous
+                plaira parmi nos jeux de rôle, alors n'hésitez pas à vous lancer
+                dans l'aventure dès maintenant !
+            </h1>
+        </div>
+    );
+}
